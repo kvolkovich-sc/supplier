55e5b1b26d90238b54d4abb9a49a18e0a3314e72
Patryk Kopycinski <contact@patrykkopycinski.com>
2017-02-14 12:40:34 +0100

    Add babel-loader search path

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -35,12 +35,12 @@ module.exports = {
   ],
 
   resolve: {
-    modules: ['node_modules'],
+    modules: [process.env.NODE_PATH, 'node_modules'],
     extensions: ['.json', '.jsx', '.js']
   },
 
   resolveLoader: {
-    modules: ['node_modules'],
+    modules: [process.env.NODE_PATH, 'node_modules'],
     extensions: ['.js']
   },
 
