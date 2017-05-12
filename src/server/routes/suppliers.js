1ba21da4afe3a539de0e67f8b09b833221b24fd8
Lekealem Asong <lekealemasong@yahoo.de>
2017-05-12 11:15:04 +0200

    Emit supplier change events to redis

--- src/server/routes/suppliers.js
+++ src/server/routes/suppliers.js
@@ -1,8 +1,11 @@
 const Supplier = require('../queries/suppliers');
+const RedisEvents = require('ocbesbn-redis-events');
 
 module.exports = function(app, db, config) {
   Supplier.init(db, config).then(() =>
   {
+    this.events = new RedisEvents({ consul : { host : 'consul' } });
+
     app.get('/api/suppliers', (req, res) => sendSuppliers(req, res));
     app.post('/api/suppliers', (req, res) => createSuppliers(req, res));
     app.get('/api/suppliers/:supplierId', (req, res) => sendSupplier(req, res));
@@ -26,22 +29,30 @@ var createSuppliers = function(req, res)
     if(exists) {
       return res.status('409').json({ message : 'A supplier already exists' });
     } else {
-      Supplier.create(newSupplier).then(supplier => {
-        const userId = supplier.createdBy;
-        const supplierId = supplier.supplierId;
-        req.ocbesbn.serviceClient.put('user', '/users/' + userId + '?tokenUpdate=true', { supplierId: supplierId, status: 'registered' })
-          .spread(() => res.status('200').json(supplier))
-          .catch(error => {
-            switch (error.response.statusCode) {
-              case 404:
-                Supplier.delete(supplierId).then(() => null);
-                res.status(404).json({ message : 'User not found!' });
-                break;
-              default:
-                res.status(400).json({ message : error.message });
-            };
-          });
-      });
+      newSupplier.status = 'new';
+      Supplier.create(newSupplier)
+        .then(supplier => this.events.emit(supplier, 'supplier').then(() => supplier))
+        .then(supplier => {
+          const userId = supplier.createdBy;
+          const supplierId = supplier.supplierId;
+          req.ocbesbn.serviceClient.put('user', '/users/' + userId + '?tokenUpdate=true', { supplierId: supplierId, status: 'registered' })
+            .spread(() => {
+              supplier.status = 'assigned';
+              Supplier.update(supplierId, supplier).then(supplier => {
+                this.events.emit(supplier, 'supplier').then(() => res.status('200').json(supplier));
+              });
+            })
+            .catch(error => {
+              switch (error.response.statusCode) {
+                case 404:
+                  Supplier.delete(supplierId).then(() => null);
+                  res.status(404).json({ message : 'User not found!' });
+                  break;
+                default:
+                  res.status(400).json({ message : error.message });
+              };
+            });
+        });
     }
   })
   .catch(e => res.status('400').json({ message : e.message }));
@@ -72,7 +83,10 @@ var updateSupplier = function(req, res)
   Supplier.exists(supplierId).then(exists =>
   {
     if(exists) {
-      return Supplier.update(supplierId, req.body).then(supplier => res.status('200').json(supplier));
+      req.body.status = 'updated';
+      return Supplier.update(supplierId, req.body).then(supplier => {
+        this.events.emit(supplier, 'supplier').then(() => res.status('200').json(supplier));
+      });
     } else {
       return res.status('404').json({ message : 'A supplier with this ID does not exist.' });
     }
