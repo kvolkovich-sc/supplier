5d5a552d19f863ebbaf24b61e390e39714d1f01d
Patryk Kopycinski <contact@patrykkopycinski.com>
2017-05-22 17:00:31 +0200

    No base image

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -27,12 +27,10 @@ module.exports = {
   ],
 
   resolve: {
-    modules: [process.env.NODE_PATH, 'node_modules'],
     extensions: ['.json', '.jsx', '.js']
   },
 
   resolveLoader: {
-    modules: [process.env.NODE_PATH, 'node_modules'],
     extensions: ['.js']
   },
 
