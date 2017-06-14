f8500625a6f188f976775de7ec88b4bafab43dfc
Lekealem Asong <lekealemasong@yahoo.de>
2017-06-14 14:36:59 +0200

    [64] Add algorithm to calculate supplier profile strength

--- src/server/routes/supplier_profile_strength.js
+++ src/server/routes/supplier_profile_strength.js
@@ -1,4 +1,35 @@
+const Supplier = require('../queries/suppliers');
+const SupplierAddress = require('../queries/supplier_addresses');
+const SupplierContact = require('../queries/supplier_contacts');
+const _ = require('underscore');
 
 module.exports = function(app, db, config) {
-  app.get('/api/suppliers/:supplierId/profile_strength', (req, res) => res.json(46));
+  app.get('/api/suppliers/:supplierId/profile_strength', (req, res) => {
+    Promise.all([
+      Supplier.init(db, config).then(() => Supplier.find(req.params.supplierId)),
+      SupplierAddress.init(db, config).then(() => SupplierAddress.all(req.params.supplierId)),
+      SupplierContact.init(db, config).then(() => SupplierContact.all(req.params.supplierId))
+    ]).
+    then(([supplier, addresses, contacts]) => {
+      let suppliers = [];
+      if (!_.isEmpty(supplier)) suppliers = [supplier];
+      const recordsArray = [suppliers, addresses, contacts];
+
+      const averages = _.map(recordsArray, (records) => {
+        if (_.isEmpty(records)) return 0;
+
+        const sum = records.length;
+
+        return _.reduce(records, (memo, record) => { return memo + recordAverage(record); }, 0) / sum;
+      });
+
+      const average = _.reduce(averages, (memo, num) => { return memo + num; }, 0) / recordsArray.length;
+      return res.json(Math.round(average * 100));
+    });
+  });
+}
+
+function recordAverage(record) {
+  const dataValues = _.values(record.dataValues);
+  return _.filter(dataValues, (num) => !_.isNull(num)).length / dataValues.length;
 }
